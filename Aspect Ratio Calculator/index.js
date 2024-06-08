137e123acad3366ad5d297016df469f659cf5bfd
Garv2003 <aggarwalgarv0505@gmail.com>
2024-06-08 17:47:14 +0530

    MAKE PROJECT

--- Aspect Ratio Calculator/index.js
+++ Aspect Ratio Calculator/index.js
@@ -1,17 +1,17 @@
-let ratioWidth = document.getElementById("ratio-width");
-let ratioHeight = document.getElementById("ratio-height");
-let width = document.getElementById("width");
-let height = document.getElementById("height");
+const ratioWidth = document.getElementById("ratio-width");
+const ratioHeight = document.getElementById("ratio-height");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
 
-let calculateWidth = () => {
-    let aspectRatio = ratioWidth.value / ratioHeight.value;
-    width.value = parseFloat((height.value * aspectRatio).toFixed(2));
+const calculateWidth = () => {
+  const aspectRatio = ratioWidth.value / ratioHeight.value;
+  width.value = parseFloat((height.value * aspectRatio).toFixed(2));
 };
 
-let calculateHeight = () => {
-    let aspectRatio = ratioWidth.value / ratioHeight.value;
-    height.value = parseFloat((width.value / aspectRatio).toFixed(2));
-}
+const calculateHeight = () => {
+  const aspectRatio = ratioWidth.value / ratioHeight.value;
+  height.value = parseFloat((width.value / aspectRatio).toFixed(2));
+};
 
 height.addEventListener("input", calculateWidth);
 width.addEventListener("input", calculateHeight);
